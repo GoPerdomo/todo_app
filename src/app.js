9155f52db6ae4b5645e2ade4df9515cb04a606c5
Gabriel de O. Perdomo <goperdomo@gmail.com>
2017-10-19 14:49:45 +0200

    Made adding new items possible

--- src/app.js
+++ src/app.js
@@ -10,32 +10,42 @@ const elementGenerator = (el, text, newClassName) => {
     }
   }
   return html;
-}
+};
+
+const renderList = (ul, list) => {
+  for(let i = 0; i < list.length; i++) {
+    ul.appendChild(list[i]);
+  }
+};
 
 // Wait till the dom is rendered
 document.addEventListener("DOMContentLoaded", function () {
   const root = document.querySelector('#root');
   const wrapper = elementGenerator('div', '', 'todo-app');
   const ul = document.createElement('ul');
+  const list = [elementGenerator('li', 'Checkbox'),
+                elementGenerator('li', 'Line-through'),
+                elementGenerator('li', 'Delete button')];
   const form = elementGenerator('form', '', 'form-wrapper');
   const input = elementGenerator('input');
   const button = elementGenerator('button', 'Add to List')
-
   
   wrapper.appendChild(elementGenerator('h1', "Things I've Gotta Get Done", 'header'));
-
   root.appendChild(wrapper);
-
   wrapper.appendChild(ul)
-
-  ul.appendChild(elementGenerator('li', 'test1'));
-  ul.appendChild(elementGenerator('li', 'test2'));
-  ul.appendChild(elementGenerator('li', 'test3'));
-
   wrapper.appendChild(form);
 
   input.placeholder = "What do I need to do today?";
   form.appendChild(input);
   form.appendChild(button);
-  
+
+  renderList(ul, list);
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    list.push(elementGenerator('li', input.value));
+    input.value = '';
+    renderList(ul, list);
+  });
+
 });
