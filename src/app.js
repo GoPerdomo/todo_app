304ac96aa5bd124c6d8b211b9c9533450664a7aa
Gabriel de O. Perdomo <goperdomo@gmail.com>
2017-10-19 13:35:17 +0200

    Adding input and button

--- src/app.js
+++ src/app.js
@@ -1,29 +1,41 @@
 import 'normalize.css';
 import './style.css';
 
+const elementGenerator = (el, text, newClassName) => {
+  const html = document.createElement(el);
+  if(text !== undefined) {
+    html.innerHTML = text;
+    if(newClassName !== undefined) {
+      html.className = newClassName;
+    }
+  }
+  return html;
+}
+
 // Wait till the dom is rendered
 document.addEventListener("DOMContentLoaded", function () {
   const root = document.querySelector('#root');
-  
-  const generateWrapper = (wrapperClass) => {
-    const html = document.createElement('div');
-    html.className = wrapperClass;
-    return html;
-  }
+  const wrapper = elementGenerator('div', '', 'todo-app');
+  const ul = document.createElement('ul');
+  const form = elementGenerator('form', '', 'form-wrapper');
+  const input = elementGenerator('input');
+  const button = elementGenerator('button', 'Add to List')
 
-  const generateElement = (el, text, newClassName) => {
-    const html = document.createElement(el);
-    if(text !== undefined) {
-      html.innerHTML = text;
-      if(newClassName !== undefined) {
-        html.className = newClassName;
-      }
-    }
-    return html;
-  }
   
-  const wrapper = generateWrapper('todo-app');
-  wrapper.appendChild(generateElement('h1', 'Things I\'ve Gotta Get Done', 'header'));
+  wrapper.appendChild(elementGenerator('h1', "Things I've Gotta Get Done", 'header'));
 
   root.appendChild(wrapper);
+
+  wrapper.appendChild(ul)
+
+  ul.appendChild(elementGenerator('li', 'test1'));
+  ul.appendChild(elementGenerator('li', 'test2'));
+  ul.appendChild(elementGenerator('li', 'test3'));
+
+  wrapper.appendChild(form);
+
+  input.placeholder = "What do I need to do today?";
+  form.appendChild(input);
+  form.appendChild(button);
+  
 });
