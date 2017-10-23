fe366b630655347d2fa882214c0cd9485d91868b
Gabriel de O. Perdomo <goperdomo@gmail.com>
2017-10-23 11:47:30 +0200

    Modifying on effects

--- src/jquery.js
+++ src/jquery.js
@@ -6,17 +6,31 @@ import 'jquery-ui-bundle';
 $('#root').hide();
 
 $(document).ready( () => {
+    const $todo =  $('.todo-app');
     $('#root').show();
-    $('.todo-app').children().hide();
-    $('.todo-app').css('display', 'none');
-    $('.todo-app').show('fold', 'slow');
+    $todo.css('display', 'none').children().hide();;
+    $todo.show('fold', 1000);
 
     setTimeout(function() {
-        $('.header').show('fade', 'slow');
+        $('.header').show('fade', 1000);
+
         setTimeout(function() {
-            $('ul').show('drop', {direction: 'right'}, 'slow');
-            $('.todo-app form').show('drop', {direction: 'left'}, 'slow');
-        }, 500);
+            const $li = $('ul').children();
+            $li.hide();
+            $('ul').show();
+            for(let i = 1; i <= $li.length*2; i++) {
+                const $current = $(`ul li:nth-child(${i})`);
+                if($li.index($current) % 2 !== 0) {
+                    $current.show('drop', {direction: 'right'}, 600);
+                } else {
+                    $current.show('drop', {direction: 'left'}, 600);
+                }
+            }
+
+            setTimeout(function() {
+                $('.todo-app form').show('slide', {direction: 'down'}, 600);
+            }, 1000);
+        }, 1000);
     }, 1000);
 });
 
